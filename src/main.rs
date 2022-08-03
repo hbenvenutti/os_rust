930e2bd15b988b6dcc84b6ea0ea8cca08a58b3f3
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-08-03 20:43:25 -0300

    criado o panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+use core::panic::PanicInfo;
+// disables the standard library. A OS cannot use a os abstraction library.
+
+/// This function is called on panic.
+// #[panic_handler]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+fn main() {}
